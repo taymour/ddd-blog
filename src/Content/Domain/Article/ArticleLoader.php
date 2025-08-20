5376311bb74c2003e88d03cf2623fab387247d4f
Taymour-NRJ <tnegib@externe.nrjgroup.fr>
2025-08-20 17:59:53 +0200

    fix use class

--- src/Content/Domain/Article/ArticleLoader.php
+++ src/Content/Domain/Article/ArticleLoader.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Content\Domain\Article;
 
 use App\Content\Domain\Exception\ArticleLoadingException;
-use App\Content\Domain\Exception\ArticleNotFoundException;
 use App\Content\Domain\Model\Article;
 use App\Content\Domain\Storage\ArticleStorageInterface;
 
